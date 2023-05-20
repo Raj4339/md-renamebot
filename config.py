7d0b7e3a7fb3d97bacc69ba228e50ad96e75c3ee
Raj4339 <121564356+Raj4339@users.noreply.github.com>
2023-05-20 12:00:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 class Config(object):
     BANNED_USERS = []
     DOWNLOAD_LOCATION = "./DOWNLOADS" 
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH" "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
-    AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", None)
-    DATABASE_URI = os.environ.get("DATABASE_URI", None)
+    API_ID = int(os.environ.get("API_ID", "24079851")
+    API_HASH = os.environ.get("API_HASH" "a4eee8cd21bf557b7a75840e8d6ef6df")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5993298723:AAHcYVVW9zWpd5XbtQchvFcuUM6kqmhzPj0")
+    OWNER_ID = int(os.environ.get("OWNER_ID", "Raj4339"))
+    AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "1001958947052")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Rajbot:Rajbot@cluster0.ifgyoay.mongodb.net/?retryWrites=true&w=majority")
